6165e6ff22b7d55e781a86f6bc1c020a9707a78c
hariharu1221 <jua.park1@gmail.com>
2021-10-29 11:22:45 +0900

    3

--- Assets/Scripts/SceneScript/StartSceneUI.cs
+++ Assets/Scripts/SceneScript/StartSceneUI.cs
@@ -1,13 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-using System.Data;
-using Mono.Data.SqliteClient;
-using System.IO;
-using UnityEngine.Networking;
 
-public class test : MonoBehaviour
+public class StartSceneUI : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
